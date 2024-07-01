053c953dfdece907ac7bd426e4a7cd3a260b3381
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-07-01 15:15:34 -0300

    configurando a leitura para ler dados vindo de um DB

--- src/main/java/com/rodrigo/springbatch/jdbccursorreader/reader/JdbcCursorReaderConfig.java
+++ src/main/java/com/rodrigo/springbatch/jdbccursorreader/reader/JdbcCursorReaderConfig.java
@@ -3,13 +3,25 @@ package com.rodrigo.springbatch.jdbccursorreader.reader;
 
 import com.rodrigo.springbatch.jdbccursorreader.dominio.Cliente;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JdbcCursorReaderConfig {
     @Bean
-    public JdbcCursorItemReader<Cliente> jdbcCursorReader() {
-        return null;
+    public JdbcCursorItemReader<Cliente> jdbcCursorReader(
+            @Qualifier("appDataSource")DataSource dataSource // o banco appDataSource setadon na classe DataSourceConfig
+            ) {
+        return new JdbcCursorItemReaderBuilder<Cliente>()
+                .name("jdbcCursorReader")
+                .dataSource(dataSource)
+                .sql("select * from cliente")
+                .rowMapper(new BeanPropertyRowMapper<Cliente>(Cliente.class))
+                .build();
     }
 }
