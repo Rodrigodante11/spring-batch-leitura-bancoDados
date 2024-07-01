427e103921500898a8fb716b3e4fe31072469b38
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-07-01 14:11:25 -0300

    start project

---
+++ src/test/java/com/rodrigo/springbatch/jdbccursorreader/JdbcCursorReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.jdbccursorreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcCursorReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
